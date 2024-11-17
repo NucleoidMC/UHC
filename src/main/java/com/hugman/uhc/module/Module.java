2cd65b73f9a559ac2e9f900f7ab105373b54b536
Hugman <contact.hugman@gmail.com>
2024-11-17 12:22:49 +0100

    Refactor modules and UHCRun data-pack folders

--- src/main/java/com/hugman/uhc/module/Module.java
+++ src/main/java/com/hugman/uhc/module/Module.java
@@ -2,45 +2,42 @@ package com.hugman.uhc.module;
 
 import com.hugman.uhc.modifier.Modifier;
 import com.hugman.uhc.registry.UHCRegistryKeys;
-import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryCodecs;
 import net.minecraft.registry.entry.RegistryElementCodec;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
+import xyz.nucleoid.codecs.MoreCodecs;
+import xyz.nucleoid.plasmid.api.util.PlasmidCodecs;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public record Module(String translation, Optional<Either<String, List<String>>> description, Item icon, TextColor color,
-                     List<Modifier> modifiers) {
+public record Module(
+        Text name,
+        Optional<Text> description,
+        Optional<List<Text>> longDescription,
+        ItemStack icon,
+        TextColor color,
+        List<Modifier> modifiers
+) {
     public static final Codec<Module> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Codec.STRING.fieldOf("translation").forGetter(Module::translation),
-            Codec.either(Codec.STRING, Codec.STRING.listOf()).optionalFieldOf("description").forGetter(Module::description),
-            Registries.ITEM.getCodec().optionalFieldOf("icon", Items.BARRIER).forGetter(Module::icon),
+            PlasmidCodecs.TEXT.fieldOf("name").forGetter(Module::name),
+            PlasmidCodecs.TEXT.optionalFieldOf("description").forGetter(Module::description),
+            MoreCodecs.listOrUnit(PlasmidCodecs.TEXT).optionalFieldOf("long_description").forGetter(Module::longDescription),
+            MoreCodecs.ITEM_STACK.optionalFieldOf("icon", new ItemStack(Items.BARRIER)).forGetter(Module::icon),
             TextColor.CODEC.optionalFieldOf("color", TextColor.fromRgb(3791743)).forGetter(Module::color),
             Modifier.TYPE_CODEC.listOf().fieldOf("modifiers").forGetter(Module::modifiers)
     ).apply(instance, Module::new));
 
     public static final Codec<RegistryEntry<Module>> ENTRY_CODEC = RegistryElementCodec.of(UHCRegistryKeys.MODULE, CODEC);
     public static final Codec<RegistryEntryList<Module>> ENTRY_LIST_CODEC = RegistryCodecs.entryList(UHCRegistryKeys.MODULE, CODEC);
-    public static final Codec<List<RegistryEntryList<Module>>> ENTRY_LISTS_CODEC = RegistryCodecs.entryList(UHCRegistryKeys.MODULE, CODEC, true).listOf();
 
-    public List<String> getDescriptionLines() {
-        List<String> list = new ArrayList<>();
-        if (description.isPresent()) {
-            Either<String, List<String>> either = description.get();
-            either.ifLeft(list::add);
-            either.ifRight(list::addAll);
-        } else {
-            list.add(translation() + ".description");
-        }
-        return list;
-    }
 }
