782ab2d8dee539063678b53a929d3009c26564e8
Hugman <contact.hugman@gmail.com>
2025-09-10 17:29:19 +0200

    Remove Ultimate Lucky Block

--- src/datagen/java/fr/hugman/uhc/impl/data/UHCDataGenerator.java
+++ src/datagen/java/fr/hugman/uhc/impl/data/UHCDataGenerator.java
@@ -1,7 +1,5 @@
 package fr.hugman.uhc.impl.data;
 
-import fr.hugman.ultimate_lucky_block.api.datagen.provider.*;
-import fr.hugman.ultimate_lucky_block.api.registry.ULBRegistryKeys;
 import fr.hugman.uhc.UHC;
 import fr.hugman.uhc.api.datagen.provider.*;
 import fr.hugman.uhc.api.registry.UHCRegistryKeys;
@@ -26,29 +24,6 @@ public class UHCDataGenerator implements DataGeneratorEntrypoint {
         // - World Generation
         pack.addProvider(UHCConfiguredFeatureProvider::new);
         pack.addProvider(UHCPlacedFeatureProvider::new);
-
-        //TEMP
-        pack.addProvider(ULBModelProvider::new);
-
-        // - Lucky Block
-        pack.addProvider(ULBEventProvider::new);
-        pack.addProvider(ULBPoolEventProvider::new);
-
-        // - UHC
-        pack.addProvider(ULBUHCConfigProvider::new);
-        pack.addProvider(ULBGameProvider::new);
-        pack.addProvider(ULBUHCModuleProvider::new);
-        pack.addProvider(ULBUHCModuleTagProvider::new);
-
-        // - Tags
-        pack.addProvider(ULBEventTagProvider::new);
-
-        // - World Generation
-        pack.addProvider(ULBConfiguredFeatureProvider::new);
-        pack.addProvider(ULBPlacedFeatureProvider::new);
-
-        // - Loot Tables
-        pack.addProvider(ULBLootTableProvider::new);
     }
 
     @Override
@@ -61,21 +36,6 @@ public class UHCDataGenerator implements DataGeneratorEntrypoint {
         // - World Generation
         registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, UHCConfiguredFeatureProvider::register);
         registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, UHCPlacedFeatureProvider::register);
-
-        //TEMP
-
-        // - Lucky Block
-        registryBuilder.addRegistry(ULBRegistryKeys.LUCKY_EVENT, ULBEventProvider::register);
-        registryBuilder.addRegistry(ULBRegistryKeys.LUCKY_EVENT, ULBPoolEventProvider::register);
-
-        // - UHC
-        registryBuilder.addRegistry(GameConfigs.REGISTRY_KEY, ULBGameProvider::register);
-        registryBuilder.addRegistry(UHCRegistryKeys.UHC_CONFIG, ULBUHCConfigProvider::register);
-        registryBuilder.addRegistry(UHCRegistryKeys.UHC_MODULE, ULBUHCModuleProvider::register);
-
-        // - World Generation
-        registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ULBConfiguredFeatureProvider::register);
-        registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ULBPlacedFeatureProvider::register);
     }
 
     @Override
